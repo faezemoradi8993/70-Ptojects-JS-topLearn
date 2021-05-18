a1958b5264b2eda93fadfabd8b682ad6b3728d16
faeze moradi <faeze.moradi8993@gmail.com>
2021-05-18 13:39:19 +0430

    p2-simple-slideshow

---
+++ p2/assets/js/App.js
@@ -0,0 +1,11 @@
+ slider=(link)=>{
+    document.getElementById("slider").src="./assets/videos"+link;
+//    let options= document.getElementsByClassName("option");
+//    this.classList.add("active");
+//    options.forEach(x => {
+       
+//     x.classList.remove("active") }); 
+}
+// removeActive=(x)=>{
+//     x.classList.remove("active")
+// }
