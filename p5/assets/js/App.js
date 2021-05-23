37b4e2d3246e80df05778a7d3690089f905ede92
faeze moradi <faeze.moradi8993@gmail.com>
2021-05-24 03:59:50 +0430

    p5

--- p5/assets/js/App.js
+++ p5/assets/js/App.js
@@ -1,19 +1,6 @@
-const bar = document.querySelector('.progress-bar');
-
-const totop = document.querySelector('.totop');
-window.addEventListener('scroll', function () {
-    // progress bar 
-    const winScroll = window.pageYOffset;
-    const height = document.documentElement.scrollHeight - window.innerHeight;
-    const scrolled = (winScroll / height) * 100;
-    bar.style.width = `${scrolled}%`;
-    //totop
-    if (winScroll > 200) {
-        totop.classList.add("active");
-
-    } 
-    else {
-        totop.classList.remove("active");
-
-    }
+const nav = document.querySelector("#nav");
+const icon = document.querySelector("#menuIcon");
+icon.addEventListener('click', ()=>{
+nav.classList.toggle('active');
+icon.classList.toggle('active');
 })
